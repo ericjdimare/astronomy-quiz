dfa8765c6d6e8f121c429ae80a8b838f84502846
Eric <ericjdimare@gmail.com>
2020-01-23 19:20:12 -0500

    Cleaned dry repetative code, replaced with helper functions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let counter = 60;
 $("#start-quiz").click(function() {
   $("#start-quiz-page").hide();
   $("#q-1").removeClass("d-none");
+
+  // Start Timer
   let timer = setInterval(function() {
     counter--;
     $("#counter").text(counter);
@@ -12,164 +14,137 @@ $("#start-quiz").click(function() {
     }
   }, 1000);
 
-  // Question 1
-  $("#93-billion").click(function() {
+  // Helper Functions
+  function incorrect() {
+    $("#correct").addClass("d-none");
+    $("#incorrect").removeClass("d-none");
+    counter -= 10;
+  }
+  function correct() {
     $("#correct").removeClass("d-none");
+    $("#incorrect").addClass("d-none");
+  }
+  function changeQuestionOne() {
     $("#q-1").hide();
     $("#q-2").removeClass("d-none");
     $("#placeholder").hide();
+  }
+  function changeQuestionTwo() {
+    $("#q-2").hide();
+    $("#q-3").removeClass("d-none");
+    $("#placeholder").hide();
+  }
+  function changeQuestionThree() {
+    $("#q-3").hide();
+    $("#q-4").removeClass("d-none");
+    $("#placeholder").hide();
+  }
+  function changeQuestionFour() {
+    $("#q-4").hide();
+    $("#q-5").removeClass("d-none");
+    $("#placeholder").hide();
+  }
+  function lastQuestion() {
+    $("#q-5").hide();
+    $("#end-game").removeClass("d-none");
+    $("#score").text(counter);
+    clearInterval(timer);
+  }
+
+  // Question 1
+  $("#93-billion").click(function() {
+    correct();
+    changeQuestionOne();
   });
 
   $("#46-billion").click(function() {
-    counter -= 10;
-    $("#correct").addClass("d-none");
-    $("#incorrect").removeClass("d-none");
-    $("#q-1").hide();
-    $("#q-2").removeClass("d-none");
-    $("#placeholder").hide();
+    incorrect();
+    changeQuestionOne();
   });
   $("#13-billion").click(function() {
-    counter -= 10;
-    $("#correct").addClass("d-none");
-    $("#incorrect").removeClass("d-none");
-    $("#q-1").hide();
-    $("#q-2").removeClass("d-none");
-    $("#placeholder").hide();
+    incorrect();
+    changeQuestionOne();
   });
   $("#4-billion").click(function() {
-    counter -= 10;
-    $("#correct").addClass("d-none");
-    $("#incorrect").removeClass("d-none");
-    $("#q-1").hide();
-    $("#q-2").removeClass("d-none");
-    $("#placeholder").hide();
+    incorrect();
+    changeQuestionOne();
   });
 
   //Question 2
+
   $("#mercury").click(function() {
-    counter -= 10;
-    $("#correct").addClass("d-none");
-    $("#incorrect").removeClass("d-none");
-    $("#q-2").hide();
-    $("#q-3").removeClass("d-none");
+    incorrect();
+    changeQuestionTwo();
   });
   $("#venus").click(function() {
-    counter -= 10;
-    $("#correct").addClass("d-none");
-    $("#incorrect").removeClass("d-none");
-    $("#q-2").hide();
-    $("#q-3").removeClass("d-none");
+    incorrect();
+    changeQuestionTwo();
   });
   $("#saturn").click(function() {
-    $("#correct").removeClass("d-none");
-    $("#incorrect").addClass("d-none");
-    $("#q-2").hide();
-    $("#q-3").removeClass("d-none");
+    correct();
+    changeQuestionTwo();
   });
   $("#neptune").click(function() {
-    counter -= 10;
-    $("#correct").addClass("d-none");
-    $("#incorrect").removeClass("d-none");
-    $("#q-2").hide();
-    $("#q-3").removeClass("d-none");
+    incorrect();
+    changeQuestionTwo();
   });
 
   // Question 3
+
   $("#oort-cloud").click(function() {
-    $("#correct").removeClass("d-none");
-    $("#incorrect").addClass("d-none");
-    $("#q-3").hide();
-    $("#q-4").removeClass("d-none");
+    correct();
+    changeQuestionThree();
   });
 
   $("#asteroid-belt").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-3").hide();
-    $("#q-4").removeClass("d-none");
+    incorrect();
+    changeQuestionThree();
   });
   $("#kuiper-belt").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-3").hide();
-    $("#q-4").removeClass("d-none");
+    incorrect();
+    changeQuestionThree();
   });
   $("#proxima-centauri").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-3").hide();
-    $("#q-4").removeClass("d-none");
+    incorrect();
+    changeQuestionThree();
   });
 
   //Question 4
 
   $("#2").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-4").hide();
-    $("#q-5").removeClass("d-none");
+    incorrect();
+    changeQuestionFour();
   });
   $("#4").click(function() {
-    $("#correct").removeClass("d-none");
-    $("#incorrect").addClass("d-none");
-    $("#q-4").hide();
-    $("#q-5").removeClass("d-none");
+    correct();
+    changeQuestionFour();
   });
   $("#6").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-4").hide();
-    $("#q-5").removeClass("d-none");
+    incorrect();
+    changeQuestionFour();
   });
   $("#8").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-4").hide();
-    $("#q-5").removeClass("d-none");
+    incorrect();
+    changeQuestionFour();
   });
 
   // Question 5
 
   $("#gas-giant").click(function() {
-    $("#correct").removeClass("d-none");
-    $("#incorrect").addClass("d-none");
-    $("#q-5").hide();
-    $("#end-game").removeClass("d-none");
-    $("#score").text(counter);
-    clearInterval(timer);
+    correct();
+    lastQuestion();
   });
   $("#terrestrial").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-5").hide();
-    $("#end-game").removeClass("d-none");
-    $("#score").text(counter);
-    clearInterval(timer);
+    incorrect();
+    lastQuestion();
   });
   $("#closer").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-5").hide();
-    $("#end-game").removeClass("d-none");
-    $("#score").text(counter);
-    clearInterval(timer);
+    incorrect();
+    lastQuestion();
   });
   $("#further").click(function() {
-    counter -= 10;
-    $("#incorrect").removeClass("d-none");
-    $("#correct").addClass("d-none");
-    $("#q-5").hide();
-    $("#end-game").removeClass("d-none");
-    $("#score").text(counter);
-    clearInterval(timer);
+    incorrect();
+    lastQuestion();
   });
 });
 
